a0e205c1d12c9603d5a6fdefb0d2cc6600f36c92
Tan Theng Lee <thengleegit@gmail.com>
2018-05-30 01:30:42 +0800

    Let user add annotations to video

---
+++ web/channels/video_channel.ex
@@ -0,0 +1,44 @@
+defmodule Rumbl.VideoChannel do
+  use Rumbl.Web, :channel
+
+  def join("videos:" <> video_id, _params, socket) do
+    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+    # :timer.send_interval(5_000, :ping)
+    # {:ok, socket}
+  end
+
+  # def handle_info(:ping, socket) do
+  #   count = socket.assigns[:count] || 1
+  #   push socket, "ping", %{count: count}
+
+  #   {:noreply, assign(socket, :count, count + 1)}
+  # end
+
+  def handle_in(event, params, socket) do
+    user = Repo.get(Rumbl.User, socket.assigns.user_id)
+    handle_in(event, params, user, socket)
+  end
+
+  def handle_in("new_annotation", params, user, socket) do
+    changeset =
+      user
+      |> build_assoc(:annotations, video_id: socket.assigns.video_id)
+      |> Rumbl.Annotation.changeset(params)
+
+    case Repo.insert(changeset) do
+      {:ok, annotation} ->
+        broadcast! socket, "new_annotation", %{
+          id: annotation.id,
+          user: Rumbl.UserView.render("user.json", %{user: user}),
+          body: annotation.body,
+          at: annotation.at
+        }
+        {:reply, :ok, socket}
+
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
+
+
+  end
+end
